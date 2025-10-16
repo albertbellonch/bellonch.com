afbaacffc5bcbaa55db0edf2aac3611c2715269c
Albert Bellonch Llargués <albert@bellonch.com>
2025-10-16 13:52:35 +0200

    Monkeypatch Sprockets submodules to make it work

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -1,1 +1,50 @@
 require "jekyll-assets"
+
+# Ensure compatibility with Jekyll 4 which expects static files to respond to `write?`
+if defined?(Sprockets::StaticAsset) && !Sprockets::StaticAsset.method_defined?(:write?)
+  class Sprockets::StaticAsset
+    def write?
+      true
+    end
+
+    # Jekyll 4 expects static files to respond to `path`
+    def path
+      if respond_to?(:pathname) && pathname
+        pathname.to_s
+      elsif respond_to?(:filename)
+        filename.to_s
+      elsif respond_to?(:logical_path)
+        logical_path.to_s
+      else
+        to_s
+      end
+    end
+  end
+end
+
+# Also patch BundledAsset which Jekyll may iterate over as a static file
+if defined?(Sprockets::BundledAsset)
+  unless Sprockets::BundledAsset.method_defined?(:write?)
+    class Sprockets::BundledAsset
+      def write?
+        true
+      end
+    end
+  end
+
+  unless Sprockets::BundledAsset.method_defined?(:path)
+    class Sprockets::BundledAsset
+      def path
+        if respond_to?(:pathname) && pathname
+          pathname.to_s
+        elsif respond_to?(:filename)
+          filename.to_s
+        elsif respond_to?(:logical_path)
+          logical_path.to_s
+        else
+          to_s
+        end
+      end
+    end
+  end
+end
